e6e436c0981cb062a88f3235dde319f203bbfebf
Mike Eads <me@mikeads.com>
2019-04-13 10:54:47 -0700

    capturing changes

--- bin/build.sh
+++ bin/build.sh
@@ -3,9 +3,7 @@
 set -ex
 
 
-if [[ "$(ls -A client/dist)" ]]; then
-    rm client/dist/*
-fi
+# if [[ "$(ls -A client/dist)" ]]; then
+#     rm client/dist/*
+# fi
 tsc && browserify client/dist/index.js -o client/dist/main.js -d
-
-
---
+++ server/src/main.ts
@@ -0,0 +1,90 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const PORT = 8080
+
+app.use(bodyParser.json({ type: "application/json" }))
+app.use(cors())
+app.options("*", cors())
+
+class DataRepository {
+    data = new Map()
+    lastId = 0
+    insert(item: any) {
+        item["id"] = ++this.lastId
+        item.lastUpdated = new Date()
+        this.data.set(this.lastId, item)
+        return this.lastId
+    }
+    update(item: any) {
+        if (!("id" in item)) {
+            this.insert(item) // not checking for duplication here so...
+        }
+        if (!this.data.has(item.id)) {
+            return false
+        }
+        let el = this.data.get(item.id)
+        for (let k of Object.keys(item)) {
+            if (k == "id") continue
+            el[k] = item[k]
+        }
+        el.lastUpdated = new Date()
+        this.data.set(el.id, el)
+        return true
+    }
+    find(id: any) {
+        return this.data.get(id)
+    }
+    findAll() {
+        return Array.from(this.data.values())
+    }
+    remove(id: any) {
+        if (!this.data.has(id)) {
+            return false
+        }
+        return this.data.delete(id)
+    }
+}
+let plantRepository = new DataRepository()
+var plantRouter = express.Router()
+
+plantRouter.get("/", (req: any, res: any) => {
+    res.statusCode = 200
+    res.json(plantRepository.findAll())
+})
+
+plantRouter.get("/:id", (req: any, res: any) => {
+    let id = parseInt(req.params.id, 10)
+    let plant = plantRepository.find(id)
+    if (!plant) {
+        res.statusCode = 404
+        res.json({ errors: ["Plant not found"] })
+    } else {
+        res.statusCode = 200
+        res.json(plant)
+    }
+})
+
+plantRouter.post("/", (req: any, res: any) => {
+    let lastId = plantRepository.insert(req.body)
+    res.statusCode = 201
+    res.json(plantRepository.find(lastId))
+})
+
+plantRouter.put("/:id", (req: any, res: any) => {
+    plantRepository.update(req.body)
+    res.statusCode = 202
+    res.json(null)
+})
+
+plantRouter.delete("/:id", (req: any, res: any) => {
+    let id = parseInt(req.params.id, 10)
+    plantRepository.remove(id)
+    req.statusCode = 200
+    res.json(null)
+})
+
+app.use("/plant", plantRouter)
+
+app.listen(PORT, () => console.log(`Example api listening on port ${PORT}!`))
