eb1fd4a32fe3df73a447754f3abfe169098581de
Mike Eads <me@mikeads.com>
2020-04-01 14:01:28 -0500

    reduce npm scripts to just 'npm run test' :)

--- bin/build-lib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "building shared library."
-echo
-
-tsc --build lib/tsconfig.json
--- bin/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "running tsc build and browserify'ing the client outFile"
-tsc --build && browserify client/dist/index.js -o client/dist/main.js -d
