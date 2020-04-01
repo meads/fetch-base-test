72e791b56e1c1ff45a650c1514f8c0230f0e243f
Mike Eads <me@mikeads.com>
2020-04-01 13:58:51 -0500

    refactor test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-echo "cleaning up lib build files."
-./bin/clean-lib.sh
+echo "removing tsconfig.tsbuildinfo file from lib."
+echo
+rm lib/tsconfig.tsbuildinfo 2> /dev/null
 
-echo "cleaning up client and server build files."
-./bin/clean.sh
+echo "removing dist directory from lib."
+echo
+rm -rf lib/dist 2> /dev/null
 
-echo "transpiling .ts files in lib project"
-./bin/build-lib.sh
 
-echo "transpiling .ts files in client/ and server/ 'typescript projects'"
-./bin/build.sh
+echo "removing tsconfig.tsbuildinfo files from client and server"
+echo
+rm client/tsconfig.tsbuildinfo 2> /dev/null
+rm server/tsconfig.tsbuildinfo 2> /dev/null
+
+echo "removing dist directories from client and server"
+echo
+rm -rf client/dist server/dist 2> /dev/null
+
+
+echo "building shared library."
+echo
+tsc --build lib/tsconfig.json
+
+echo "running tsc build and browserify'ing the client outFile"
+tsc --build && browserify client/dist/index.js -o client/dist/main.js -d
+echo
+
 
 echo "starting server."
 node ./server/dist/main.js &
+echo
 
 echo "sleeping a sec while node starts the process."
 sleep 1
+echo
 
 echo "starting a local http server in the client/ directory."
 http-server ./client -c-1 -p 8081 -o
